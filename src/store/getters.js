19c7f52b1a494da8e045d7da21c6bf76be6d719c
gongzigege <wjg301234@aliyun.com>
2021-01-03 20:51:42 +0800

    format

--- src/store/getters.js
+++ src/store/getters.js
@@ -1,6 +1,6 @@
 const getters = {
-  appbar: state => state.app.appbar,
-  visitedTabViews: state => state.tabViews.visitedTabViews,
+  appbar: (state) => state.app.appbar,
+  visitedTabViews: (state) => state.tabViews.visitedTabViews
 }
 
 export default getters
